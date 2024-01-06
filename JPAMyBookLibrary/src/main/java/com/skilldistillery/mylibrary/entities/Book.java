ee88e2e938c208437bafad057de0a851aa04c5f3
Johnny Smyth <johnnysmyth@Johnnys-MacBook-Pro.local>
2024-01-05 16:56:23 -0800

    configure jpa project, test for book entity

---
+++ JPAMyBookLibrary/src/main/java/com/skilldistillery/mylibrary/entities/Book.java
@@ -0,0 +1,108 @@
+package com.skilldistillery.mylibrary.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	private String author;
+	private String genre;
+	private String format;
+	private Integer length;
+	private String description;
+	
+	public Book() {
+		super();
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+	
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	public String getFormat() {
+		return format;
+	}
+	
+	public void setFormat(String format) {
+		this.format = format;
+	}
+	
+	public Integer getLength() {
+		return length;
+	}
+	
+	public void setLength(Integer length) {
+		this.length = length;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return id == other.id;
+	}
+	
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", author=" + author + ", genre=" + genre + ", format=" + format
+				+ ", length=" + length + ", description=" + description + "]";
+	}
+	
+	
+}
---
+++ JPAMyBookLibrary/src/test/java/com/skilldistillery/mylibrary/entities/BookTest.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.mylibrary.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+class BookTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Book book;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAMyBookLibrary");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		book = em.find(Book.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		book = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(book);
+		assertEquals("Name of the Wind", book.getName());
+	}
+
+}
